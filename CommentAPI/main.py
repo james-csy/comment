4484d8b23449570af9fc334499d73a2476b9cd41
James Chong <98079863+james-csy@users.noreply.github.com>
2024-06-04 19:44:27 +0000

    dummy data including one nested comment

--- CommentAPI/main.py
+++ CommentAPI/main.py
@@ -8,12 +8,25 @@ app = FastAPI()
 #Temporary Dummy Database
 db: List[Comment] = [
     Comment(
-        commentID=uuid4(),
+        commentID="ac598b80-a96e-4848-b292-d84bcc8554e1",
         parentCommentID=None,
-        childrenCommentIDs=None,
+        childrenCommentIDs=["34c8117f-2700-43db-8568-71151c8ac5c4"], #check if this List format is valid
         comment="This is a test comment",
         author="james.chong@aetna.com",
         datetime="June 6th 2024",
+        tags=["jason.chong@aetna.com", "jameschong@berkeley.edu"],
+        importance=Importance.notImportant,
+        category=Category.fixMe,
+        status=Status.open,
+        context=uuid4() #replace with context id in the future
+    ),
+    Comment(
+        commentID="34c8117f-2700-43db-8568-71151c8ac5c4",
+        parentCommentID="ac598b80-a96e-4848-b292-d84bcc8554e1",
+        childrenCommentIDs=None,
+        comment="This is also a test comment",
+        author="james.chong@aetna.com",
+        datetime="June 7th 2024",
         tags=["jason.chong@aetna.com", "jameschong@berkeley.edu"],
         importance=Importance.notImportant,
         category=Category.fixMe,
