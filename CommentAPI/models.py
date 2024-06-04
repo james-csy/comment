79d9b5dbe97e42a4a5bdf671b94d2743d64b4a25
James Chong <98079863+james-csy@users.noreply.github.com>
2024-06-04 19:36:20 +0000

    change folder name

---
+++ CommentAPI/models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from uuid import UUID, uuid4
+from enum import Enum
+
+class Importance(str, Enum):
+    notImportant = "Not Important"
+    important = "Important"
+
+class Category(str, Enum):
+    fixMe = "Fix Me"
+    other = "Other"
+    
+class Status(str, Enum):
+    open = "Open"
+    resolved = "Resolved"
+    
+class Comment(BaseModel):
+    commentID: Optional[UUID] = uuid4()
+    parentCommentID: Optional[UUID] = None
+    childrenCommentIDs: Optional[List[UUID]] = List[None]
+    comment: str
+    author: str
+    datetime: str
+    tags: List[str]
+    importance: Optional[Importance] = Importance.notImportant
+    category: Optional[Category] = Category.other
+    status: Optional[Status] = Status.open
+    Context: Optional[UUID] = None #context for where the comment is
